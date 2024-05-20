88c78617931505e7005a31f330407d06be6b3dfa
MonashJeremia <jyov0001@student.monash.edu>
2024-05-20 16:58:43 +1000

    Added commentation
    select_sites should be reviewed to reduce repetition of initializing the MaxHeap again

--- mode1.py
+++ mode1.py
@@ -11,13 +11,13 @@ class Mode1Navigator:
         Best Case is O(1) if one or no landsites were added
         Worst Case is O(N*log(N)) where N is the number of land sites and log(N) is the complexity per insertion operation in a heap
         """
-        self.sites = sites
-        self.adventurers = adventurers
-        self.site_heap = MaxHeap(len(sites))
+        self.sites = sites # Stores a list of 'Land' data classes
+        self.adventurers = adventurers 
+        self.site_heap = MaxHeap(len(sites)) # Initialises MaxHeap with the length of sites, used to maintain land sites based on gold to guardian ratio
 
-        for site in sites:
-            ratio = site.get_gold() / site.get_guardians()
-            self.site_heap.add((ratio, site))
+        for site in sites: # Iterates through the 'Lands'
+            ratio = site.get_gold() / site.get_guardians() # Calculates the gold to guardian ratio
+            self.site_heap.add((ratio, site)) # Adds to the MaxHeap, with ratio as the key to be utilised by the MaxHeap and have site as the value
 
     def select_sites(self) -> list[tuple[Land, int]]:
         """
@@ -28,23 +28,22 @@ class Mode1Navigator:
         Worst Case is O(N) where N is the number of land sites, as when all the the adventurers 
         have to be distributed linearly across multiple sites until all adventurers are distributed
         """
-        selected_sites = []
+
+        selected_sites = [] # Initialise an empty list to store [land and adventurers]
         remaining_adventurers = self.adventurers
 
-        # Initialize a max-heap based on the gold-to-guardians ratio
-        heap = MaxHeap(len(self.sites))
+        heap = MaxHeap(len(self.sites)) # Initialises another Maxheap 
         for site in self.sites:
             ratio = site.get_gold() / site.get_guardians()
             heap.add((ratio, site))
 
-        while remaining_adventurers > 0 and len(heap) > 0:
-            # Extract the site with the highest gold-to-guardians ratio
-            ratio, best_site = heap.get_max()
-            max_adventurers_for_site = min(best_site.get_guardians(), remaining_adventurers)
-            selected_sites.append((best_site, max_adventurers_for_site))
-            remaining_adventurers -= max_adventurers_for_site
+        while remaining_adventurers > 0 and len(heap) > 0: # Keep looping when there are still adventurers and Lands remaining
+            ratio, best_site = heap.get_max() # Extracts Land with the largest gold to guardian ratio
+            max_adventurers_for_site = min(best_site.get_guardians(), remaining_adventurers) # Allocates the adventurers to the amount of guardians of the Land
+            selected_sites.append((best_site, max_adventurers_for_site)) # Append to selected_sites 
+            remaining_adventurers -= max_adventurers_for_site # Subtract the adventurers that was just allocated from the remaining adventurers
 
-        return selected_sites
+        return selected_sites # Return List
 
     def select_sites_from_adventure_numbers(self, adventure_numbers: list[int]) -> list[float]:
         """
